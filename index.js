9ced758f7c03089c9ec19569db2131b6541f14e3
Bianca <biancancanal@gmail.com>
2021-03-29 16:46:20 -0500

    Complete

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
+document.addEventListener('DOMContentLoaded', function() {
+  fetchBooks();
+});
+
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  return fetch ('https://anapioficeandfire.com/api/books') 
+    .then(resp => resp.json())
+    .then(data => {
+      renderBooks(data);
+    });
 }
+
+//fetchBooks();
 
 function renderBooks(books) {
   const main = document.querySelector('main');
@@ -12,6 +21,4 @@ function renderBooks(books) {
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
+
